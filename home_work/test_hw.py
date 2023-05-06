08b410d8692be8c3801e74399cdca595827f00fd
Olifiya <Oliffisa@yandex.ru>
2023-05-06 21:56:20 +0300

    full

---
+++ home_work/test_hw.py
@@ -0,0 +1,9 @@
+def test_passing():
+    assert ("home", "work") == ("home", "work")
+
+def test_passing2():
+    assert ("QA" == "QC")
+
+def test_passing3():
+    assert not (1, 2, 3, 5) == (2, 3, 5)
+
